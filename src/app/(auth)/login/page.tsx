3dfa4fffa6b5d1ec2987bda9557bb4c36fdbc3a5
Vlad <vladmelnik193@gmail.com>
2025-07-07 16:57:08 +0300

    feat: wrap login and signup forms in a container for improved layout and styling

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -28,37 +28,39 @@ export default function Page() {
   }
 
   return (
-    <form
-      onSubmit={handleFormSubmit}
-      className='py-12 px-8 border border-[#DEDFE1] rounded-[10px] max-w-[616px] w-full mx-auto mt-8'
-    >
-      <h1 className='text-[#3D3D3D] text-4xl font-bold leading-[45px] mb-4'>
-        Login
-      </h1>
-      <p className='text-[#949494] leading-5 mb-[52px]'>
-        Do not have an account,{' '}
-        <Link href='/signup' className='text-[#2B2B2B] underline'>
-          create a new one.
-        </Link>
-      </p>
+    <div className='container'>
+      <form
+        onSubmit={handleFormSubmit}
+        className='py-12 px-8 border border-[#DEDFE1] rounded-[10px] max-w-[616px] w-full mx-auto my-8'
+      >
+        <h1 className='text-[#3D3D3D] text-4xl font-bold leading-[45px] mb-4'>
+          Login
+        </h1>
+        <p className='text-[#949494] leading-5 mb-[52px]'>
+          Do not have an account,{' '}
+          <Link href='/signup' className='text-[#2B2B2B] underline'>
+            create a new one.
+          </Link>
+        </p>
 
-      <Input
-        className='mb-[30px]'
-        id='email'
-        name='email'
-        label='Enter your email'
-        placeholder='michael.joe@gmail.com'
-        type='email'
-      />
-      <Input
-        className='mb-[30px]'
-        id='password'
-        name='password'
-        label='Enter Your Password'
-        placeholder='michael.joe@xmail.com'
-        type='password'
-      />
-      <ConfirmBtn className=''>Login</ConfirmBtn>
-    </form>
+        <Input
+          className='mb-[30px]'
+          id='email'
+          name='email'
+          label='Enter your email'
+          placeholder='michael.joe@gmail.com'
+          type='email'
+        />
+        <Input
+          className='mb-[30px]'
+          id='password'
+          name='password'
+          label='Enter Your Password'
+          placeholder='michael.joe@xmail.com'
+          type='password'
+        />
+        <ConfirmBtn className=''>Login</ConfirmBtn>
+      </form>
+    </div>
   );
 }
